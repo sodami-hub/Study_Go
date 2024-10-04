4be0f529b5ff2aeb5bba969292b1391a478a8c99
good4ram@naver.com <good4ram@naver.com>
2024-10-04 17:57:30 +0900

    ch3 표준 입출력03

--- ch3/ex3.8/ex3.8.go
+++ ch3/ex3.8/ex3.8.go
@@ -8,12 +8,15 @@ expected integer -> 에러발생
 expected integer -> 다음 입력에도 hello의 문자열이 남아서(e) 에러가 자동 발생
 */
 import (
-	//"bufio"
+	//"bufio"	// io를 담당하는 패키지
 	"fmt"
-	//"os"
+	//"os"		// 표준 입출력 등을 가지고 있는 패키지
 )
 
+// bufio는 입력 스트림으로부터 한 줄을 읽는 Reader 객체를 제공한다.
+
 func main() {
+	// 표준 입력을 읽는 객체, NewReader() 함수는 인수로 입력되는 입력 스트림을 가지고 Reader 객체를 생성한다.
 	//hstdin := bufio.NewReader(os.Stdin)
 
 	var a int
@@ -23,6 +26,7 @@ func main() {
 
 	if err != nil {
 		fmt.Println(err)
+		// 줄 바꿈 문자가 나올 때까지 읽는다. 이렇게 하면 표준 입력 스트림이 비워진다.
 		//		stdin.ReadString('\n')
 	} else {
 		fmt.Println(n, a, b)
