421ade068516842817eba37bc46ebced63872585
good4ram@naver.com <good4ram@naver.com>
2024-10-14 16:50:25 +0900

    ch15_숫자맞추기게임만들기

---
+++ ch15/findnumber.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+func main() {
+	var stdin = bufio.NewReader(os.Stdin)
+
+	num := rand.Intn(100)
+	count := 0
+
+	var myNum int
+
+	for {
+		fmt.Print("숫자값을 입력하세요 > ")
+
+		_, err := fmt.Scanln(&myNum)
+
+		if err != nil {
+			stdin.ReadString('\n')
+			continue
+		}
+
+		count++
+
+		if num == myNum {
+			fmt.Println("정답입니다. 시도한 횟수 : ", count)
+			break
+		} else if num > myNum {
+			fmt.Println("입력하신 숫자가 작습니다.")
+		} else {
+			fmt.Println("입력하신 숫자가 더 큽니다.")
+		}
+	}
+
+}
