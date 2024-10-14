ae0df3d72cf1373ae9611d45e6442a6f95f27132
good4ram@naver.com <good4ram@naver.com>
2024-10-14 13:42:27 +0900

    구조체 초기화 방법 수정

--- ch11_구조체/ex11.1/ex11.1.go
+++ ch11_구조체/ex11.1/ex11.1.go
@@ -10,7 +10,11 @@ type House struct {
 }
 
 func main() {
+	//===== 초기화 방식 2 구조체 초기값 없이 초기화만
+	var houseInit House = House{}
+	fmt.Println(houseInit)
 
+	// ====== 초기화 방식 1 - 직접 대입?
 	var house House
 	house.Address = "경기도 성남시..."
 	house.Size = 28
@@ -22,6 +26,7 @@ func main() {
 	fmt.Printf("가격 : %0.2f억원\n", house.Price)
 	fmt.Println("타입 :", house.Type)
 
+	// 초기화 방식 3. 모든 변수 초기화
 	// 구조체 변수의 초기화는 아래와 같은 방법도 가능하다. 아래는 모든 변수를 초기화하는 경우이다.
 	var house02 House = House{
 		"서울시 서초구",
@@ -29,9 +34,9 @@ func main() {
 		25,
 		"APT", // 여러줄로 초기화하는 경우 제일 뒤에 ,를 붙이도록 한다.(한 줄로 나열할때는 상관없다.)
 	}
-
 	fmt.Println(house02.Address, house02.Price, house02.Size, house02.Type)
 
+	// 초기화 방식 4. 일부 필드값 초기화.
 	// 구조체 변수의 일부 필드값만 초기화하는 경우
 	var house03 House = House{
 		Size: 34,
