d97d0ff631c2967e9bfad75989bf0d6d1ab51b3d
good4ram@naver.com <good4ram@naver.com>
2024-10-23 16:40:21 +0900

    ch25 교제 제시된 답안 - 같은 결과

---
+++ ch25_SearchWordProject/ex25.1/ex25.1.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("2개 이상의 인수가 필요합니다.")
+		return
+	}
+
+	word := os.Args[1]
+	files := os.Args[2:]
+	fmt.Println("찾으려는 단어", word)
+
+	PrintAllFiles(files)
+}
+
+func GetFileList(path string) ([]string, error) {
+	return filepath.Glob(path)
+}
+
+func PrintAllFiles(files []string) {
+	for _, path := range files {
+		filelist, err := GetFileList(path)
+		if err != nil {
+			fmt.Println("파일 경로가 잘못됐습니다.")
+			return
+		}
+		fmt.Println("찾으려는 파일 리스트")
+		for _, name := range filelist {
+			fmt.Println(name)
+		}
+	}
+}
---
+++ ch25_SearchWordProject/ex25.3/ex25.3.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type LineInfo struct {
+	lineNo int
+	line   string
+}
+
+type FindInfo struct {
+	filename string
+	lines    []LineInfo
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("2개 이상의 실행 인수가 필요한다.")
+		return
+	}
+
+	word := os.Args[1]
+	files := os.Args[2:]
+
+	findInfos := []FindInfo{}
+
+	for _, path := range files {
+		findInfos = append(findInfos, FindWordInAllFiles(word, path)...)
+	}
+
+	for _, findInfo := range findInfos {
+		fmt.Println(findInfo.filename)
+		fmt.Println("-----------------------------------")
+		for _, lineInfo := range findInfo.lines {
+			fmt.Println("\t", lineInfo.lineNo, "\t", lineInfo.line)
+		}
+		fmt.Println("-----------------------------------")
+		fmt.Println()
+	}
+}
+
+func GetFileList(path string) ([]string, error) {
+	return filepath.Glob(path)
+}
+
+func FindWordInAllFiles(word, path string) []FindInfo {
+	findInfos := []FindInfo{}
+
+	filelist, err := GetFileList(path)
+	if err != nil {
+		fmt.Println("파일 경로 에러")
+		return findInfos
+	}
+
+	for _, filename := range filelist {
+		findInfos = append(findInfos, FindWordInFile(word, filename))
+	}
+	return findInfos
+}
+
+func FindWordInFile(word, filename string) FindInfo {
+	findInfo := FindInfo{filename, []LineInfo{}}
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("파일이 없다", filename)
+		return findInfo
+	}
+	defer file.Close()
+
+	lineNo := 1
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, word) {
+			findInfo.lines = append(findInfo.lines, LineInfo{lineNo, line})
+		}
+		lineNo++
+	}
+	return findInfo
+}
