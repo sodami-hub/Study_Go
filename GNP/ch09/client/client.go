5843ff64aea2c29afc3de004beeaf87123c57a33
ganzigood <good4ram@naver.com>
2024-12-31 06:14:55 +0900

    GNP-ch09+

--- GNP/ch09/client/client.go
+++ GNP/ch09/client/client.go
@@ -1,1 +1,59 @@
 package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type testCases struct {
+	method   string
+	body     io.Reader
+	code     int
+	Response string
+}
+
+func main() {
+	myCase := []testCases{
+		{http.MethodGet, nil, http.StatusOK, "Hello, friend!"},
+		{http.MethodPost, bytes.NewBuffer([]byte("<world>")), http.StatusOK, "Hello, &lt;world&gt;!"},
+		{http.MethodHead, nil, http.StatusMethodNotAllowed, ""},
+	}
+
+	client := new(http.Client)
+	path := fmt.Sprintf("http://localhost:8081")
+
+	for i, c := range myCase {
+		r, err := http.NewRequest(c.method, path, c.body)
+		if err != nil {
+			fmt.Println("make request error", i, err)
+			continue
+		}
+
+		resp, err := client.Do(r)
+		if err != nil {
+			fmt.Println("send req error", err)
+			continue
+		}
+		fmt.Println("expect statusCode :", c.code)
+		fmt.Println("actual statusCode :", resp.StatusCode)
+
+		if resp.StatusCode != c.code {
+			fmt.Println("status code error")
+			continue
+		}
+
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("read body error", err)
+			continue
+		}
+		_ = resp.Body.Close()
+
+		fmt.Println("expect response : ", c.Response)
+		fmt.Println("actual respons : ", string(b))
+
+	}
+
+}
--- GNP/ch09/server/server.go
+++ GNP/ch09/server/server.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/awoodbeck/gnp/ch09/handlers"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	srv := &http.Server{
+		Addr: "127.0.0.1:8081",
+		Handler: http.TimeoutHandler(
+			handlers.DefaultHandler(), 2*time.Minute, ""),
+		IdleTimeout:       5 * time.Minute,
+		ReadHeaderTimeout: time.Minute,
+	}
+
+	l, err := net.Listen("tcp", srv.Addr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		/*
+		   Serve() : http.Server 의 메서드는 서버가 종료될 때까지 블록처리된다.(고수준 서버 프로그래밍에서 사용)
+		   이 함수는 HTTP 서버를 시작하고 클라이언트의 요청을 처리하는 역할을 한다.
+		   net.Listener를 인수로 받아들이며 내부적으로 Accept() 함수를 호출하고, HTTP 요청을 처리한다.
+
+		   Accept() : net.Listener 인터페이스의 메서드로, 새로운 클라이언트 연결을 수락하는 역할을 한다.(저수준 서버 프로그래밍에서 사용)
+		   이 함수는 블로킹 호출로, 새로운 연결이 들어올 때까지 대기한다. 새로운 연결이 들어오면,
+		   net.Conn 인터페이스를 반환하여 클라이언트와의 통신을 처리할 수 있게 한다.
+		*/
+		err := srv.Serve(l)
+		if err != http.ErrServerClosed {
+			fmt.Println(err)
+		}
+	}()
+	wg.Wait()
+}
